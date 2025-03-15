bdbd4fe97291257084ea66aa9670043c2a223f91
Junha Park <junha.park@yonsei.ac.kr>
2025-03-16 02:49:57 +0900

    move news json

--- app/components/News.tsx
+++ app/components/News.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import news from "../news.json";
+import news from "@/app/data/news.json";
 
 export default function News() {
   const [page, setPage] = useState(1);
